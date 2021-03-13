eca2c8b00f53e89c4d0f5509373bc0dde6026135
Suzana96 <79999817+Suzana96@users.noreply.github.com>
2021-03-13 16:43:56 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// slider
+var $cont = document.querySelector('.cont');
+var $elsArr = [].slice.call(document.querySelectorAll('.el'));
+var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
+
+setTimeout(function() {
+  $cont.classList.remove('s--inactive');
+}, 200);
+
+$elsArr.forEach(function($el) {
+  $el.addEventListener('click', function() {
+    if (this.classList.contains('s--active')) return;
+    $cont.classList.add('s--el-active');
+    this.classList.add('s--active');
+  });
+});
+
+$closeBtnsArr.forEach(function($btn) {
+  $btn.addEventListener('click', function(e) {
+    e.stopPropagation();
+    $cont.classList.remove('s--el-active');
+    document.querySelector('.el.s--active').classList.remove('s--active');
+  });
+});
+
+
+
+// slider
+
+
+//sidebar
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+//sidebar
+
+
+//page 1
+function readMore() {
+  var expandInfo = document.getElementById("more-info-js");
+  var mainHeadings = document.getElementById("main-headings-js");
+  
+  mainHeadings.style.transform = "scale(0.7)";
+  expandInfo.style.height = "350px";
+} 
+//page 1
+
+
+
+//carousel
+
+//carousel
